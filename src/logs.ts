a41a15e408c4866d1a9a89a0450f4301071d2e64
Brian Celenza <bcelenza@gmail.com>
2023-02-05 09:23:14 -0800

    fix(format): Misc fixes to code format
    Really just trying to trigger a full NPM release to test
    semantic-release

--- src/logs.ts
+++ src/logs.ts
@@ -4,44 +4,59 @@ import {
   DescribeLogGroupsCommandOutput
 } from "@aws-sdk/client-cloudwatch-logs";
 
-const getLogGroupsByPrefix = async(logGroupPrefix: string, client: CloudWatchLogsClient): Promise<string[]> => {
-    const logGroups: string[] = [];
+const getLogGroupsByPrefix = async (
+  logGroupPrefix: string,
+  client: CloudWatchLogsClient
+): Promise<string[]> => {
+  const logGroups: string[] = [];
 
-    let response: DescribeLogGroupsCommandOutput = {
-        $metadata: {}
-    };
-    do {
-        response = await client.send(new DescribeLogGroupsCommand({
-            logGroupNamePrefix: logGroupPrefix,
-            nextToken: response.nextToken
-        }));
+  let response: DescribeLogGroupsCommandOutput = {
+    $metadata: {},
+  };
+  do {
+    response = await client.send(
+      new DescribeLogGroupsCommand({
+        logGroupNamePrefix: logGroupPrefix,
+        nextToken: response.nextToken,
+      })
+    );
 
-        const matchingLogGroups = response.logGroups;
-        if (!matchingLogGroups) {
-            throw new Error(`No log groups found for prefix ${logGroupPrefix}`);
-        }
+    const matchingLogGroups = response.logGroups;
+    if (!matchingLogGroups) {
+      throw new Error(`No log groups found for prefix ${logGroupPrefix}`);
+    }
 
-        logGroups.push(...matchingLogGroups.map(g => g.logGroupName!));
-    } while (response.nextToken)
+    logGroups.push(...matchingLogGroups.map((g) => g.logGroupName!));
+  } while (response.nextToken);
 
-    return logGroups;
-}
+  return logGroups;
+};
 
-export const expandLogGroups = async(inputLogGroupNames: string[], client: CloudWatchLogsClient): Promise<Set<string>> => {
+export const expandLogGroups = async (
+  inputLogGroupNames: string[],
+  client: CloudWatchLogsClient
+): Promise<Set<string>> => {
   const logGroupNames = new Set<string>();
   for (const inputName of inputLogGroupNames) {
-    if (inputName.includes('*')) {
+    if (inputName.includes("*")) {
       // ensure only one asterisk is provided at the end
-      if (!inputName.endsWith('*') || (inputName.match(/\*/g) || []).length > 1) {
-        throw new Error(`Could not match log group by name ${inputName}: only prefix matching is supported by CloudWatch.`);
-      } 
+      if (
+        !inputName.endsWith("*") ||
+        (inputName.match(/\*/g) || []).length > 1
+      ) {
+        throw new Error(
+          `Could not match log group by name ${inputName}: only prefix matching is supported by CloudWatch.`
+        );
+      }
 
-      const matchingLogGroups = await getLogGroupsByPrefix(inputName.replace(/\*/g, ''), client);
-      matchingLogGroups.forEach(g => logGroupNames.add(g));
+      const matchingLogGroups = await getLogGroupsByPrefix(
+        inputName.replace(/\*/g, ""),
+        client
+      );
+      matchingLogGroups.forEach((g) => logGroupNames.add(g));
     } else {
       logGroupNames.add(inputName);
     }
   }
   return logGroupNames;
-}
-
+};
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 export const unescapeValue = (v: string): string => {
-    return v.replace(new RegExp('\\\\"', 'g'),'"')
-        .replace(new RegExp('\\\\n', 'g'), '\n')
-        .trim();
-}
+  return v
+    .replace(new RegExp('\\\\"', "g"), '"')
+    .replace(new RegExp("\\\\n", "g"), "\n")
+    .trim();
+};
